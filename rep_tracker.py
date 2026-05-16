ebe4e75740cc2ed73439a095056f6b89d13708fb
Samyak Khobragade <67233107+samyak-io@users.noreply.github.com>
2026-05-16 22:42:32 +0530

    Update rep_tracker.py

--- rep_tracker.py
+++ rep_tracker.py
@@ -5,18 +5,13 @@ Stage = Literal["UP", "DOWN"]
 
 @dataclass(frozen=True)
 class RepState:
-    """
-    It tracks the current stage of the rep the person is in.
-    """
-
     count: int = 0
     stage: Stage = "UP"
-    baseline_hip_anlge: float | None = None
+    baseline_hip_angle: float | None = None
 
     form_errors: int = 0
     lowest_hip_angle: float = 180.0
     
-    # Thresholds for pushup
     UP_THRESHOLD: float = 160.0
     DOWN_THRESHOLD: float = 90.0
 
@@ -24,7 +19,6 @@ def calculate_next_state(current_state: RepState, angle: float) -> RepState:
     count = current_state.count 
     stage = current_state.stage
 
-    # 1. logic for when we are currently UP
     if stage == "UP":
         if angle < current_state.DOWN_THRESHOLD:
             print("DOWN Detected!")
